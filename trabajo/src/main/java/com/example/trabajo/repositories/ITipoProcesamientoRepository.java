d66087a581ce9157782030a6eb4450d4ab2252f3
maui705 <mauricioseminario2000@gmail.com>
2026-04-25 14:53:25 -0500

    Delete trabajo directory

--- trabajo/src/main/java/com/example/trabajo/repositories/ITipoProcesamientoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.trabajo.repositories;
-
-import com.example.trabajo.entities.TipoProcesamiento;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ITipoProcesamientoRepository extends JpaRepository<TipoProcesamiento,Integer> {
-}
