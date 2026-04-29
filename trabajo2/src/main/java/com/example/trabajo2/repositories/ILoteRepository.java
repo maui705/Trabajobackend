348df0c93862edba82e746ec85a38f8e168a9ae5
maui705 <mauricioseminario2000@gmail.com>
2026-04-28 20:28:48 -0500

    Delete trabajo2 directory

--- trabajo2/src/main/java/com/example/trabajo2/repositories/ILoteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.trabajo2.repositories;
-
-import com.example.trabajo2.entities.Lote;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ILoteRepository extends JpaRepository<Lote, Integer> {
-}
